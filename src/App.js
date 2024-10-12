5f04515b36853243a3d8bdc32f1d6191fc7bb9e5
Utkarsh Agarwal <agarwalutkarsh58@gmail.com>
2024-10-12 23:52:06 +0530

    Enabled Sidebar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Board from "./components/Board";
 import Sidebar from "./components/Sidebar";
-import { MdChevronRight } from "react-icons/md";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 function App() {
-  const handleClick = (style) => {};
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className="app">
       <NavBar />
       <div className="home">
-        <Sidebar />
+        {isOpen && <Sidebar />}
         <div className="op-sidebar">
-          <button onClick={handleClick}>
-            <MdChevronRight />
+          <button onClick={handleClick} className={`isOpen ? open : ''`}>
+            {isOpen ? <MdChevronLeft /> : <MdChevronRight />}
           </button>
         </div>
         <Board />
