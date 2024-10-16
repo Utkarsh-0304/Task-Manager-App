ed82efeaaaee3f53af656aced3e06a8bf0026bae
Utkarsh Agarwal <agarwalutkarsh58@gmail.com>
2024-10-16 23:55:10 +0530

    Improved List-Board Component

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ function App() {
     <div className="app">
       <NavBar />
       <div className="home">
-        <Sidebar isOpen={isOpen} />
+        {/* <Sidebar isOpen={isOpen} />
         <div className="op-sidebar">
           <button onClick={handleClick} className={isOpen ? "open" : ""}>
             {isOpen ? <MdChevronLeft /> : <MdChevronRight />}
           </button>
-        </div>
+        </div> */}
         <Board />
       </div>
     </div>
