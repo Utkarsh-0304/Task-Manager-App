322157f5deb0f31d7ffd764496f163ebd5378d47
Utkarsh Agarwal <agarwalutkarsh58@gmail.com>
2025-05-26 20:43:01 +0530

    Skelaton list UI changes

--- frontend/src/components/SkeletonList.jsx
+++ frontend/src/components/SkeletonList.jsx
@@ -1,9 +1,9 @@
 export const SkeletonList = () => {
   return Array.from({ length: 3 }).map((_, idx) => (
-    <div className="flex flex-col gap-[1rem] p-[1rem] min-w-[300px] shadow-2xl rounded-[5px] bg-white/50 h-fit animate-pulse">
-      <div className="animate-pulse bg-white/50 h-[20px] w-[80%]"></div>
-      <div className="animate-pulse bg-white/50 h-[20px] w-[60%]"></div>
-      <div className="animate-pulse bg-white/50 h-[20px] w-[40%]"></div>
+    <div className="flex flex-col gap-[1rem] p-[1rem] min-w-[300px] shadow-2xl rounded-[5px] bg-black/10 h-fit animate-pulse">
+      <div className="animate-pulse bg-black/50 h-[20px] w-[80%]"></div>
+      <div className="animate-pulse bg-black/30 h-[20px] w-[60%]"></div>
+      <div className="animate-pulse bg-black/20 h-[20px] w-[40%]"></div>
     </div>
   ));
 };
